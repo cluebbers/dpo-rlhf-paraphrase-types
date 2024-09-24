01903b9eb87cd151aed1a500fdc44a5714424955
GOESTERN-0893970 <u12246@gwdu102.usr.hpc.gwdg.de>
2024-09-24 13:58:53 +0200

    slurm right

--- slurm_gen.sh
+++ slurm_gen.sh
@@ -2,16 +2,39 @@
 #SBATCH --job-name=paraphrase_type_gen
 #SBATCH --account=luebbers_masters
 #SBATCH --partition=gpu
-#SBATCH -N 1
-#SBATCH --cpus-per-gpu 32
-#SBATCH --gpus 1
-#BATCH --requeue
+#SBATCH -t 12:00:00
+#SBATCH -n 1
+#SBATCH --gpus V100:1
+#SBATCH -c 1
+#SBATCH --mail-type=all              # send mail when job begins and ends
+#SBATCH --mail-user=c.luebbers@stud.uni-goettingen.de 
+#SBATCH --output=./slurm_files/slurm-%x-%j.out     # where to write output, %x give job name, %j names job id
+#SBATCH --error=./slurm_files/slurm-%x-%j.err      # where to write slurm error
 
-echo $1
-echo $2
+module load miniforge3
+module load cuda
+source activate unsloth_env
+
+# Printing out some info.
+echo "Submitting job with sbatch from directory: ${SLURM_SUBMIT_DIR}"
+echo "Home directory: ${HOME}"
+echo "Working directory: $PWD"
+echo "Current node: ${SLURM_NODELIST}"
+
+echo "Model: $1"
+echo "Task: $2"
+
+# For debugging purposes.
+python --version
+python -m torch.utils.collect_env
+nvcc -V
+python -c "import torch; print('PyTorch version:', torch.__version__)"
+echo "Current environment: $(which python)"
+echo $PATH
+
+# Set PYTHONPATH to point to the correct site-packages directory
+export PYTHONPATH=/home/uni08/hpc/c.luebbers/u12246/.conda/envs/unsloth_env/lib/python3.11/site-packages:$PYTHONPATH
 
 export TOKENIZERS_PARALLELISM=false
-
-source ~/.bashrc
 
 python3 src/finetune_generation.py --task_name $2 --model_name $1
