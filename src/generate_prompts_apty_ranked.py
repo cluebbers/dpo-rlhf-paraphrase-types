b93d281f51a0c8bb02eacdc03ef4fa4f284f9508
wsad@posteo.net <c.luebbers@stud.uni-goettingen.de>
2024-11-04 11:54:30 +0100

    prompt like jan

--- src/generate_prompts_apty_ranked.py
+++ src/generate_prompts_apty_ranked.py
@@ -143,10 +143,10 @@ def write_to_jsonl(data, filename):
             # Construct generation entry
             generation_entry = {
                 "prompt": (
-                    "Given the following sentence, generate a paraphrase with the following type. "
+                    "Instruction: Given the following sentence, generate a paraphrase with the following type. "
                     f"Sentence: {instance['original']} "
                     f"Paraphrase Type: {instance['APT']}. "
-                    "Generated Paraphrase: "
+                    "Answer: "
                 ),
                 "chosen": f"{instance['chosen']}",
                 "rejected": f"{instance['rejected']}",
